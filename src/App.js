1a84f5edb8db950574ce37cd0b91d3bb9d5f3af6
Ray Letoa <rayletoa@RAYway-2.hub>
2022-03-09 11:37:10 +1300

    bottom section done and also added new turners grey image

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import SectionOne from './Components/SectionOne';
 import SectionTwo from './Components/SectionTwo';
 import SectionThree from './Components/SectionThree';
 import SectionFour from './Components/SectionFour';
+import SectionFive from './Components/SectionFive';
+import SectionSix from './Components/SectionSix';
 import Body from './Components/Body';
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
       <SectionTwo />
       <SectionThree />
       <SectionFour />
+      <SectionFive />
+      <SectionSix />
       Mission 05 - Jennifer Ariza
       
       <Body />
---
+++ src/Components/SectionSix.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './css/SectionFour.css'
+
+export default function SectionFour() {
+  return (
+<div id="section-four-block">
+
+  <div id="section-four-content">
+
+    <div id='top-title'>
+      Frequently Asked Questions
+    </div>
+
+    <div id='search-bar'>
+      <span class="title-heading">Search function here</span><br/>
+      <input type="text" id="search" />
+    </div>
+
+    <div class="title-heading">
+      Results section here
+    </div>
+
+  </div>
+
+</div>
+  )
+}
