11eff08d15673511b5f06ae1c01a79fcaee1d710
Ray Letoa <rayletoa@RAYway-2.hub>
2022-03-09 15:55:53 +1300

    added correct images and sectin changes complete

--- src/Components/SectionTwo.js
+++ src/Components/SectionTwo.js
@@ -7,110 +7,135 @@ export default function SectionTwo() {
 
   <div id="section-two-content">
 
-    <div class='section-two-title'>
-    What we offer at Turners Cars Insurance  
-    </div>
-
-    <div class='section-two-box-two'>
-
-      <div class='section-two-left'>
-        <div id='section-two-top'>
-          <img src="Images/icon02.png" alt=''/><br/>
-            <div class='section-two-left-title'>
-            Mechanical<br/> Breakdown Insurance
-            </div>
+        <div id="product-solutions">
+        Product Solutions
         </div>
-          <div id='section-two-bottom-left'>
-            <div id='section-two-bottom-left-text'>
-              This is and insurance policy that<br/>
-              protects you from unexpected costs<br/>
-              in the event that your car has a<br/>
-              sudden and unforseen break-down.<br/>
-            </div>
-            <div id='section-two-box-two-button'>
-            Find out more
-            </div>
+
+        <div class='section-two-title'>
+        What we offer at Turners Cars Insurance  
+        </div>
+
+        <div class='section-two-box-two'>
+
+          <div class='section-two-left'>
+            <div id='left-background'>
+              <div id='section-two-top'>
+                  <img src="Images/circle-icon01.png" alt=''/><br/>
+                  <div class='section-two-left-title'>
+                  Mechanical<br/> Breakdown Insurance
+                  </div>
+              </div>
+              
+                <div id='section-two-bottom-left'>
+                  <div id='section-two-bottom-left-text'>
+                    This is and insurance policy that
+                    protects you from unexpected costs
+                    in the event that your car has a
+                    sudden and unforseen break-down.
+                  </div>
+                  <div id='section-two-read-more'>
+                    <div id='section-two-read-more-format'>
+                      <a href="#" class="read-more">Read more</a> - arrow
+                    </div>
+                  </div>
+                </div>
+              </div>
           </div>
-      </div>
 
-      <div class='section-two-right'>
-        <div id='section-two-top'>
-          <img src="Images/icon01.png" class='icon01' alt=''/><br/>
-            <div class='section-two-left-title'>
-            Third Party, Fire<br/> Accident & Theft
-            </div>
-        </div>
-          <div id='section-two-bottom-left'>
-            <div id='section-two-bottom-left-text'>
-            Cover for your car if it is damaged by<br/>
-            fire, stolen, or you accidentally<br/>
-            damage someone else's property<br/>
-            with your car.<br/>
-            </div>
 
-            <div id='section-two-box-two-button'>
-            Find out more
-            </div>
+          <div class='section-two-right'>
+            
+            <div id='left-background'>
+              <div id='section-two-top'>
+                  <img src="Images/circle-icon02.png" alt=''/><br/>
+                  <div class='section-two-left-title'>
+                  Third Party, Fire<br/>Accident & Theft
+                  </div>
+              </div>
+              
+                <div id='section-two-bottom-left'>
+                  <div id='section-two-bottom-left-text'>
+                    This is and insurance policy that
+                    protects you from unexpected costs
+                    in the event that your car has a
+                    sudden and unforseen break-down.
+                  </div>
+                  <div id='section-two-read-more'>
+                    <div id='section-two-read-more-format'>
+                      <a href="#" class="read-more">Read more</a> - arrow
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+
+          
         </div>
-      </div>
-    </div>
 
     {/* ========== */}
 
     <div id='section-two-box-three'>
-    
-    <span class='section-two-title'>Compare cover</span>
+      
+        <div id="product-solutions">
+        Compare cover
+        </div>
+
+        <div class='section-two-title'>
+        What we offer at Turners Cars Insurance  
+        </div>
 
       <div id='section-two-box-three-content'>
-
-      <table id="compare-cover-plan">
-        <tr>
-          <th class='column-one'></th>
-          <th class='column-two'>Mechanical<br/>Breakdown Insurance</th>
-          <th class='column-two'>Mechanical<br/>Breakdown Insurance</th>
-        </tr>
-        <tr>
-          <td class='info-column'>Mechanical and Electrical breakdown</td>
-          <td class='tick-column'>tick icon</td>
-          <td class='tick-column'>tick icon</td>
-        </tr>
-        <tr>
-          <td class='info-column'>Mechanical and Electrical breakdown</td>
-          <td class='tick-column'>tick icon</td>
-          <td class='tick-column'>tick icon</td>
-        </tr>
-        <tr>
-          <td class='info-column'>Mechanical and Electrical breakdown</td>
-          <td class='tick-column'>tick icon</td>
-          <td class='tick-column'>tick icon</td>
-        </tr>
-        <tr>
-          <td class='info-column'>Mechanical and Electrical breakdown</td>
-          <td class='tick-column'>tick icon</td>
-          <td class='tick-column'>tick icon</td>
-        </tr>
-        <tr>
-          <td class='info-column'>Mechanical and Electrical breakdown</td>
-          <td class='tick-column'>tick icon</td>
-          <td class='tick-column'>tick icon</td>
-        </tr>
-        <tr>
-          <td class='info-column'>Mechanical and Electrical breakdown</td>
-          <td class='tick-column'>tick icon</td>
-          <td class='tick-column'>tick icon</td>
-        </tr>
-        <tr>
-          <td class='info-column'>Mechanical and Electrical breakdown</td>
-          <td class='tick-column'>tick icon</td>
-          <td class='tick-column'>tick icon</td>
-        </tr>
-        <tr>
-          <td class='bottom-column'></td>
-          <td class='bottom-column'><div class='get-a-quote-button'>Get a quote!</div></td>
-          <td class='bottom-column'><div class='get-a-quote-button'>Get a quote</div></td>
-        </tr>
-      </table>
-
+        <div id='table-holder'>
+        
+          <table id="compare-cover-plan">
+            <tr>
+              <th class='column-one'></th>
+              <th class='column-two'>Mechanical<br/>Breakdown Insurance</th>
+              <th class='column-two'>Mechanical<br/>Breakdown Insurance</th>
+            </tr>
+            <tr>
+              <td class='info-column'>Mechanical and Electrical breakdown</td>
+              <td class='tick-column'>tick icon</td>
+              <td class='tick-column'>tick icon</td>
+            </tr>
+            <tr>
+              <td class='info-column'>Mechanical and Electrical breakdown</td>
+              <td class='tick-column'>tick icon</td>
+              <td class='tick-column'>tick icon</td>
+            </tr>
+            <tr>
+              <td class='info-column'>Mechanical and Electrical breakdown</td>
+              <td class='tick-column'>tick icon</td>
+              <td class='tick-column'>tick icon</td>
+            </tr>
+            <tr>
+              <td class='info-column'>Mechanical and Electrical breakdown</td>
+              <td class='tick-column'>tick icon</td>
+              <td class='tick-column'>tick icon</td>
+            </tr>
+            <tr>
+              <td class='info-column'>Mechanical and Electrical breakdown</td>
+              <td class='tick-column'>tick icon</td>
+              <td class='tick-column'>tick icon</td>
+            </tr>
+            <tr>
+              <td class='info-column'>Mechanical and Electrical breakdown</td>
+              <td class='tick-column'>tick icon</td>
+              <td class='tick-column'>tick icon</td>
+            </tr>
+            <tr>
+              <td class='info-column'>Mechanical and Electrical breakdown</td>
+              <td class='tick-column'>tick icon</td>
+              <td class='tick-column'>tick icon</td>
+            </tr>
+          </table>
+          <div class='reveal-all'>
+            <a href="#" >Show all items</a> - icon
+          </div>
+        </div>
+          <div class='quote-now'>
+            <button>Get a quote now!</button>
+          </div>
       </div>
 
     </div>  
